e583efb109dfd51632ede5986d1c9cfec9ade716
Sandrine Le Mentec <sandrine.lementec@gmail.com>
2018-03-26 18:04:57 +0200

    premier import du projet proxibanque

---
+++ ProxiBanque_1_Sandrine/source/src/Proxibanque/model/CompteEpargne.java
@@ -0,0 +1,26 @@
+package Proxibanque.model;
+
+/**
+ *Classe CompteEpargne : classe fille de la classe compte.
+ *les instances de cette classe sont attribués à un seul et unique client.
+ * @author Sandrine
+ * @version 0.1
+ *
+ */
+
+
+public class CompteEpargne extends Compte {
+
+	
+	public CompteEpargne(String numeroCompte, double solde, String type){
+		super();
+	}
+	
+	public CompteEpargne() {
+		
+	}
+
+	
+	
+
+}
